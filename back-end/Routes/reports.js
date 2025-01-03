1dee96cc8848252139c20d9fc256b872114fcf15
kosinth <kosinth.bua@gmail.com>
2025-01-03 14:44:43 +0700

    add new module product reports

--- back-end/Routes/reports.js
+++ back-end/Routes/reports.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mysql = require('mysql2/promise');
 const conn = require('../config/configdb');
 
-router.post('/report/sellreport/:dateparam', async(req, res) => {
+router.post('/report/sell/:dateparam', async(req, res) => {
 
     let sell = req.params.dateparam
     const db = await conn('TestDB');
@@ -20,11 +20,11 @@ router.post('/report/sellreport/:dateparam', async(req, res) => {
             //console.log(sqlStr)
             const results = await db.query(`${sqlStr}`)
             res.setHeader('Content-Type', 'text/plain');
-            console.log('file:reports.js[ api:/report/sellreport ] generate reports sell ok -->' ,results[0])
+            console.log('file:reports.js[ api:/report/sell ] generate reports sell ok -->' ,results[0])
             return res.status(200).send(results[0])
 
         }catch(err){
-            console.error('Error : file:reports.js[ api:/report/sellreport ]-->' ,err.message);
+            console.error('Error : file:reports.js[ api:/report/sell ]-->' ,err.message);
             res.status(500).json({
                 err : 'มีข้อผิดพลาด : ',
                 msg : err.message
@@ -34,10 +34,62 @@ router.post('/report/sellreport/:dateparam', async(req, res) => {
     }else{
         res.status(500).json({
             err : 'มีข้อผิดพลาด : ',
-            msg : 'Error:file name->sell.js|path api post[/sell/create]|Connection to Database fail ---> Error Access denied'   
+            msg : 'Error:file name->report.js|path api post[/report/sell]|Connection to Database fail ---> Error Access denied'   
         })
     } 
 
 });
 
+router.post('/report/product/:dateparam', async(req, res) => {
+
+    let sell = req.params.dateparam
+    const db = await conn('TestDB');
+
+    if(db){
+        try{
+            const arrDateParam = sell.split(',')
+            console.log('Report sell date from to  : ',arrDateParam[0] + " to " + arrDateParam[1])
+            
+            let sqlStr = "SELECT COUNT(a.prodt_id) as cnt ,b.prodt_name ,a.prodt_id "
+            sqlStr +=  " FROM tbOrder_details a ,tbProduct b "
+            sqlStr +=  ` where DATE_FORMAT(a.order_timestamp, '%Y-%m-%d') between DATE_FORMAT('${arrDateParam[0]}', '%Y-%m-%d') `
+            sqlStr +=  ` and DATE_FORMAT('${arrDateParam[1]}', '%Y-%m-%d') `
+            sqlStr +=  " and a.prodt_id = b.prodt_id "
+            sqlStr +=  " GROUP by a.prodt_id "
+            sqlStr +=  " ORDER BY b.prodt_name DESC "
+            //console.log(sqlStr)
+           
+            const results = await db.query(`${sqlStr}`)
+
+            res.setHeader('Content-Type', 'text/plain');
+            console.log('file:reports.js[ api:/report/product ] generate reports product ok -->' ,results[0])
+            return res.status(200).send(results[0])
+
+        }catch(err){
+            console.error('Error : file:reports.js[ api:/report/product ]-->' ,err.message);
+            res.status(500).json({
+                err : 'มีข้อผิดพลาด : ',
+                msg : err.message
+            })
+        }
+
+    }else{
+        res.status(500).json({
+            err : 'มีข้อผิดพลาด : ',
+            msg : 'Error:file name->report.js|path api post[/report/product]|Connection to Database fail ---> Error Access denied'   
+        })
+    } 
+
+});
+
+
+
 module.exports = router;
+
+
+// SELECT a.prodt_id,a.prodt_qty ,b.prodt_name 
+// FROM tbOrder_details a ,tbProduct b
+// where DATE_FORMAT(a.order_timestamp, '%Y-%m-%d') between DATE_FORMAT('2024-12-25', '%Y-%m-%d')   and DATE_FORMAT('2025-1-2', '%Y-%m-%d')  
+// and a.prodt_id = b.prodt_id
+// GROUP by a.prodt_id,a.prodt_qty
+// order by a.prodt_qty DESC
--- front-end/js/report.js
+++ front-end/js/report.js
@@ -19,40 +19,45 @@ onActive =(paramIn)=>{
 
     let domFromdate = document.querySelector('input[name=fromdate]').value
     let domTodate = document.querySelector('input[name=todate]').value
+    //console.log(' SSS -->  ', domFromdate + "  " + domTodate )
+
     let arrStrFrmDate = domFromdate.split('/')
     let convtFromdate = parseInt(arrStrFrmDate[2])
     convtFromdate = convtFromdate - 543
     const  dateFrm = convtFromdate+ "-" + arrStrFrmDate[1] + "-" + arrStrFrmDate[0]
-    //console.log(' HHHH ', arrStrFrmDate[0] + "/" + arrStrFrmDate[1] + "/" + dateFrm )
-
+    //console.log(' HHHH ', dateFrm )
+   
     let arrStrToDate = domTodate.split('/')
     let convtTodate = parseInt(arrStrToDate[2])
     convtTodate = convtTodate - 543
-    const  dateTo = convtFromdate + "-" + arrStrToDate[1] + "-" + arrStrToDate[0]
+    const  dateTo = convtTodate + "-" + arrStrToDate[1] + "-" + arrStrToDate[0]
+    //console.log(' XXX ', dateTo)
 
     if(paramIn=='sell'){
-
         document.getElementById('dateinfo').innerText = " วันที่ " +domFromdate + "   -   "+ domTodate  
-        const resultArr  =   showChart(dateFrm,dateTo);
+        showChart(dateFrm,dateTo);
 
     }else{
-        //alert(paramIn)
+       //alert(paramIn)
+       document.getElementById('dateinfoProdt').innerText = " วันที่ " +domFromdate + "   -   "+ domTodate  
+       showChartProduct(dateFrm,dateTo);
+    
     }
-
 
 }
 
 
-const showChart= async(datefrmIn,datetoIn)=>{
+showChart= async(datefrmIn,datetoIn)=>{
 
     let errmsg = document.getElementById('errMsg')
+    errmsg.innerHTML='';
     let datefrm_to = datefrmIn+","+datetoIn
     const arr = [];
 
     try{
       //console.log(' Send Date from to XXX :  ',datefrm_to)
-      const response = await axios.post(`http://localhost:5000/api/report/sellreport/${datefrm_to}`)
-      console.log('reports :',response.data)
+      const response = await axios.post(`http://localhost:5000/api/report/sell/${datefrm_to}`)
+      //console.log('reports :',response.data)
       arr.push(response.data)
       await generateReport(arr);
 
@@ -71,7 +76,7 @@ const showChart= async(datefrmIn,datetoIn)=>{
         errmsg.style.color = 'red'
         
     }  
-    return arr;
+    //return arr;
   
 }
 
@@ -96,7 +101,7 @@ generateReport = async(arrParam)=>{
     let cntSell =0
     let arrday=[]
     let arrtotal=[]
-    let arrcolor=[]
+    //let arrcolor=[]
 
      for(let row=0;row<3;row++){
         v_innerHtml += ` <tr>`;
@@ -116,7 +121,7 @@ generateReport = async(arrParam)=>{
             switch(row) {
                 case 0:
                     let dateCnvtTh = convertDateTH(arrParam[0][i].Day)
-                    v_innerHtml += `<td>${dateCnvtTh} </td>`;
+                    v_innerHtml += `<td style="background-color:rgb(177, 200, 250););">${dateCnvtTh} </td>`;
                     arrday.push(dateCnvtTh)
                     break;
                 case 1:
@@ -126,7 +131,7 @@ generateReport = async(arrParam)=>{
                     break;
                   case 2:
                     sumTotal += parseFloat(arrParam[0][i].total)
-                    v_innerHtml += `<td>${setAmountFormatTh(arrParam[0][i].total)} </td>`;
+                    v_innerHtml += `<td style="background-color:rgb(176, 198, 243);">${setAmountFormatTh(arrParam[0][i].total)} </td>`;
                     arrtotal.push(parseFloat(arrParam[0][i].total))
                     break;
               }
@@ -144,6 +149,7 @@ generateReport = async(arrParam)=>{
 
 
 }
+
 
 convertDateTH = (dateIn)=>{
 
@@ -215,3 +221,144 @@ generateSellChart = (dayin,totalin)=>{
     });
 
 }
+
+generateProductChart = (arrIn)=>{
+
+    
+    let v_innerHtml="";
+    let domTable = document.getElementById('tblProductReport')
+    let colorMax=[]
+
+     for(let row=0;row<2;row++){
+        v_innerHtml += ` <tr>`;
+        switch(row) {
+            case 0:
+                v_innerHtml += `<td class='tdReport' >สินค้า</td>`;
+                break;
+            case 1:
+                v_innerHtml += `<td class='tdReport' >จำนวน</td>`;
+                break;
+        }
+        for(let i=0;i<arrIn[0].length;i++){
+            //console.log( ' COl : xx  ',arrParam[0][i].cntOrder)
+            switch(row) {
+                case 0:
+                    v_innerHtml += `<td style="background-color:rgb(177, 200, 250););">${arrIn[0][i].prodt_name} </td>`;
+                    //arrday.push(dateCnvtTh)
+                    break;
+                case 1:
+                    //cntSell += parseInt(arrIn[0][i].cntOrder)
+                    v_innerHtml += `<td>${arrIn[0][i].cnt} </td>`;
+                    colorMax.push(arrIn[0][i].cnt)
+                    //arrtotal.push(arrParam[0][i].cntOrder)
+                    break;
+              }
+        }
+        v_innerHtml += `  </tr>`;
+    }
+
+    domTable.innerHTML=v_innerHtml
+    
+    //console.log(arrIn)
+    let max= Math.max.apply(Array, colorMax);
+    console.log(' Max :  ',max)
+
+    let arrColor=[];
+    let arrPrdt=[];
+    let arrPrdtCnt=[];
+    let icntColor=1;
+    let totalProduct =0;
+    for(let j=0;j<arrIn[0].length;j++){
+        //console.log(' ArrIN :  ', arrIn[0][j].cnt + " "+ arrIn[0][j].prodt_name)
+        arrPrdt.push(arrIn[0][j].prodt_name)
+        totalProduct += parseInt(arrIn[0][j].cnt)
+        arrPrdtCnt.push(arrIn[0][j].cnt)
+        if(max==arrIn[0][j].cnt){
+            arrColor.push("#1e7145")
+        }else{
+            switch (icntColor){
+                case 1:
+                    arrColor.push("#b91d47")
+                    break;
+                case 2:
+                    arrColor.push("#00aba9")
+                    break;
+                break;
+                case 3:
+                    arrColor.push("#2b5797")
+                    break;
+                case 4:
+                    arrColor.push("orange")
+                    break;
+                case 5:
+                    arrColor.push("#e8c3b9")
+                    break;
+            }
+            if(icntColor==5){
+                icntColor = 0
+            }
+            icntColor++;
+        }
+
+    }
+    document.getElementById('sumCntProduct').innerText = setAmoutFormat(totalProduct)
+    let pieChartContent = document.getElementById('pieChartContentProduct');
+    pieChartContent.innerHTML = '&nbsp;';
+    $('#pieChartContentProduct').append('<canvas id="productChart" ><canvas>');
+     
+    new Chart("productChart", {
+    type: "bar",
+    data: {
+        labels: arrPrdt,
+        datasets: [{
+            backgroundColor: arrColor,
+            data: arrPrdtCnt
+        }]
+        },
+        options: {
+        legend: {display: false},
+        title: {
+            display: true
+            //text: "World Wine Production 2018"
+            }
+        }
+    });
+
+}
+
+
+
+ showChartProduct= async(datefrmIn,datetoIn)=>{
+
+    let errmsg = document.getElementById('errMsg')
+    errmsg.innerHTML = '';
+    let datefrm_to = datefrmIn+","+datetoIn
+    const arr = [];
+
+    try{
+      //console.log(' Send Date from to XXX :  ',datefrm_to)
+      const response = await axios.post(`http://localhost:5000/api/report/product/${datefrm_to}`)
+      //console.log('reports :',response.data)
+      arr.push(response.data)
+    //await generateReportProduct(arr);
+    await generateProductChart(arr)
+
+    }catch(err){
+        let messageErr = ''
+        if(err.response){
+            console.log(err.message)
+            messageErr = err.response.data.err + " " +err.response.data.msg
+            messageErr = err.message
+            //errmsg.style.color = 'red'
+        }else{
+            messageErr = 'มีข้อผิดพลาด: ' +err.message+ "---> ไม่สามารถเชื่อมต่อ Server ได้...! "
+            console.log(messageErr)
+        }
+        errmsg.innerText = messageErr
+        errmsg.style.color = 'red'
+        
+    }  
+  
+}
+
+
