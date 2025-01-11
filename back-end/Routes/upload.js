2b73d44a60e15dc9fbc51ffeb294e4b837dfdab5
kosinth <kosinth.bua@gmail.com>
2025-01-11 22:47:08 +0700

    add module download file

--- back-end/Routes/upload.js
+++ back-end/Routes/upload.js
@@ -5,19 +5,23 @@ const conn = require('../config/configdb');
 const dt = require('../config/system')
 const multer = require('multer');
 const fs = require('fs')
-const XLSX = require('xlsx')
+const XLSX = require('xlsx');
+const http = require('http');
+
+
+const { console } = require('inspector');
 let filename =''
 
 const storage = multer.diskStorage({
     destination :   function(req,file,cb){
         cb(null,'uploads')
     },filename : function(req,file,cb){
-        const newDate = dt(); 
-        filename = `${newDate}_${file.originalname}`
+        // const newDate = dt(); 
+        // filename = `${newDate}_${file.originalname}`
+        filename = file.originalname
+        console.log(filename)
         cb(null,filename)
-        console.log(' upload Success :  ',filename)
-
-
+        console.log(' upload file ---> ok :  ',filename)
     }
 
 })
@@ -27,16 +31,12 @@ const upload = multer({
 
 router.post('/upload',upload.single('loadfile') ,async(req, res) => {
 
-    const db = await conn('TestDB');
-
-    if(db){
         try{
             console.log('file:upload.js[ api:/upload ] upload ok --> ')
-            
-            const scuu = await readTextFile(filename)
-            //const succ =  await reeadExcel(filename)
+            //const scuu = await readTextFile(filename)
+            const succ =  await reeadExcel(filename)
             res.status(200).json({
-                message:  req.file
+                message:  'Upload Success'
             })
 
         }catch(err){
@@ -47,30 +47,41 @@ router.post('/upload',upload.single('loadfile') ,async(req, res) => {
             })
         }
 
-    }else{
-        res.status(500).json({
-            err : 'มีข้อผิดพลาด : ',
-            msg : 'Error:file name->upload.js|path api post[/upload]| Upload fail ---> Error Access denied'   
-        })
-    } 
-
 });
 
-const reeadExcel= (fileIn)=>{
+const reeadExcel= async (fileIn)=>{
 
-    try{
-        path = require('path')   
-        let filePath = path.join('uploads', 'Refsnes', '..', fileIn);
-        console.log( ' FFF ',filePath)
-   
-        const workbook = XLSX.readFile(`${filePath}`);
-        const sheet_name_list = workbook.SheetNames;
-        const xlData = XLSX.utils.sheet_to_txt(workbook.Sheets[sheet_name_list[0]]);
-        console.log(xlData);
+    const db = await conn('TestDB');
+    if(db){
+        try{
+            path = require('path')   
+            let filePath = path.join('uploads', 'Refsnes', '..', fileIn);
+            //console.log( ' FFF ',filePath)
+            const workbook = XLSX.readFile(`${filePath}`);
+            const sheet_name_list = workbook.SheetNames;
+            const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+            //console.log(xlData )
+            //delete data form product Table 
+            const resultsdel = await db.query('TRUNCATE TABLE  tbProduct;')
+            console.log('file:upload.js[ api:/upload ] truncate table tbProduct ok --> ')
+            for(let i = 0;i<xlData.length;i++){
+                let sql = `INSERT INTO tbProduct (prodt_name, prodt_price,prodt_short) VALUES ('${xlData[i].สินค้า}', '${xlData[i].ราคาต่อหน่วย}','${xlData[i].คีย์ค้นหา}')`;
+                const results = await db.query(sql)
+            }
+            console.log('file:upload.js[ api:/upload ] insert to tbProduct ok --> ')
+            db.end();
 
-    }catch(error){
-        console.error('Error:file name->upload.js|path api post[/upload]| ',error);
-    }
+        }catch(error){
+            console.error('Error:file name->upload.js|path api post[/upload]| ',error);
+            db.end();
+        }
+
+    }else{
+        res.status(500).json({
+           err : 'มีข้อผิดพลาด : ',
+           msg : 'Error:file name->upload.js|path api post[/upload]| Upload fail ---> Error Access denied'   
+        })
+    } 
  }
 
 const readTextFile =(fileIn)=>{
@@ -86,6 +97,29 @@ const readTextFile =(fileIn)=>{
         }
     });
 }
+
+router.get('/download', async(req, res, next) => {        
+
+    const path = require('path')   
+    const mime = require('mime-types')
+    const file = 'download' + "/product.xlsx"
+    console.log(file)
+    const fileName = path.basename(file)
+    const mimeType =  mime.contentType(file)
+    res.setHeader("Content-Disposition", "attachment;filename=" + fileName)
+    res.setHeader("Content-Type", mimeType)
+    console.log('file:upload.js[ api:/download ] download file ok --> ')
+
+    setTimeout(() => {
+        res.download(file)
+    }, 2000);
+
+
+
+});
+
+
+
 
 module.exports = router;
 
---
+++ front-end/js/upload.js
@@ -0,0 +1,59 @@
+const uploadFile = async () =>{
+
+    const fileInput = document.getElementById('fileInput')
+    const progressBar = document.getElementById('uploadProgress')
+    const uploadPercentageDisplay = document.getElementById('uploadPercentage')
+
+    if (!fileInput.files.length) {
+        return JSAlert.alert('กรุณาเลือก File...!')
+    }
+
+    const formData = new FormData();
+    formData.append('loadfile', fileInput.files[0]);
+    try{
+        const response = await axios
+        .post(`http://localhost:5000/api/upload`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          onUploadProgress: function(progressEvent) {
+            // เพิ่ม update progress กลับเข้า UI ไป
+            const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            progressBar.value = percentCompleted
+            uploadPercentageDisplay.innerText = `${percentCompleted}%`
+          },
+        })
+
+        console.log(response.data)
+        JSAlert.alert('Upload file สำเร็จ');
+    
+    }catch( err){
+        console.log(err.message)
+        JSAlert.alert('มีข้อผิดพลาด : Upload file ไม่สำเร็จ');
+
+    }
+
+
+}
+
+
+const downloadFile = async () => {
+  try{
+    await axios({
+      url: 'http://localhost:5000/api/download',
+      method: 'GET',
+      responseType: 'blob',
+    }).then((response) => {
+       const url = window.URL.createObjectURL(new Blob([response.data]));
+       const link = document.createElement('a');
+       link.href = url;
+       link.setAttribute('download', 'Product.xlsx');
+       document.body.appendChild(link);
+       link.click();
+       JSAlert.alert('Download file สำเร็จ');
+      
+    });
+  } catch(error){
+    console.log(error)
+  }
+};
